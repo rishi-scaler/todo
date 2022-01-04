090cd7f410d885da4b6cc8fae98a1068aea67561
Rishi Kumar <rishi.kumar@scaler.com>
2022-01-04 09:44:07 +0530

    mark as complete and undo action for todo items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,9 +1,19 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
  
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
     redirect_to @todo_list
+  end
+
+  def complete
+    if @todo_item.completed_at.blank?
+      @todo_item.update_attribute(:completed_at, Time.now)
+    else
+      @todo_item.update_attribute(:completed_at, "")
+    end
+    redirect_to @todo_list, notice: "Todo item completed"
   end
 
   def destroy
@@ -22,6 +32,10 @@ class TodoItemsController < ApplicationController
   @todo_list = TodoList.find(params[:todo_list_id])
  end
 
+ def set_todo_item
+  @todo_item = @todo_list.todo_items.find(params[:id])
+ end
+ 
  def todo_item_params
   params[:todo_item].permit(:content)
  end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root 'todo_lists#index'
   
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items do 
+      member do
+        patch :complete
+      end
+    end
   end
 end
